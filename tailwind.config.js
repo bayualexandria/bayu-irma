56ad14269311ade3aa80a899df243199b4b6e8a0
bayualexandria <wardanabayu455@gmail.com>
2023-08-27 21:37:32 +0900

    My first upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        textPrimary: "#f14e95",
+      },
+    },
+    fontFamily: {
+      sacramento: ["Sacramento"],
+    },
+  },
+  plugins: [],
+};
